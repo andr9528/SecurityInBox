a7f373c642fb7104ee710921a3e23a9c5ae3571f
atmar kohistany <atmar@hotmail.dk>
2018-03-21 13:57:56 +0100

    Atmar: Create a page and navigation

--- Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile.Android/Resources/Resource.designer.cs
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile.Android/Resources/Resource.designer.cs
@@ -2738,17 +2738,20 @@ namespace SecurityInAMobile.Droid
 			// aapt resource value: 0x7f020109
 			public const int notification_icon_background = 2130837769;
 			
-			// aapt resource value: 0x7f02010c
-			public const int notification_template_icon_bg = 2130837772;
-			
 			// aapt resource value: 0x7f02010d
-			public const int notification_template_icon_low_bg = 2130837773;
+			public const int notification_template_icon_bg = 2130837773;
+			
+			// aapt resource value: 0x7f02010e
+			public const int notification_template_icon_low_bg = 2130837774;
 			
 			// aapt resource value: 0x7f02010a
 			public const int notification_tile_bg = 2130837770;
 			
 			// aapt resource value: 0x7f02010b
 			public const int notify_panel_notification_icon_bg = 2130837771;
+			
+			// aapt resource value: 0x7f02010c
+			public const int security4 = 2130837772;
 			
 			static Drawable()
 			{
