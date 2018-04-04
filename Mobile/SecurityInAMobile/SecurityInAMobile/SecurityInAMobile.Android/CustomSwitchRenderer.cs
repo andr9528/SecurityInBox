bd12c6e98f89903edebe8148c163a0d3ecffc0cd
andr9528 <andre@steenhoff.dk>
2018-04-04 12:48:05 +0200

    More Custom...

--- Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile.Android/CustomSwitchRenderer.cs
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile.Android/CustomSwitchRenderer.cs
@@ -9,10 +9,15 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SecurityInAMobile.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
+[assembly: ExportRenderer(typeof(Android.Widget.Switch), typeof(CustomSwitchRenderer))]
 namespace SecurityInAMobile.Droid
 {
-    class CustomSwitchRenderer
+    public class CustomSwitchRenderer : SwitchRenderer
     {
+
     }
 }
