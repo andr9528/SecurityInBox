24868a8fccde61f92184b31783bc6a79b6c9ed71
andr9528 <andre@steenhoff.dk>
2018-04-06 12:27:17 +0200

    moved around files, and tried to get custom renderers to work

---
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/CustomPickerRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SecurityInAMobile;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
+namespace SecurityInAMobile
+{
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        public CustomPickerRenderer(Context context) : base(context)
+        {
+
+        }
+    }
+}
--- Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/CustomSwitchRenderer.cs
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/CustomSwitchRenderer.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using SecurityInAMobile.Droid;
+using SecurityInAMobile;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
 [assembly: ExportRenderer(typeof(Android.Widget.Switch), typeof(CustomSwitchRenderer))]
-namespace SecurityInAMobile.Droid
+namespace SecurityInAMobile
 {
     public class CustomSwitchRenderer : SwitchRenderer
     {
-
+        public CustomSwitchRenderer(Context context) : base(context)
+        {
+            
+        }
     }
 }
