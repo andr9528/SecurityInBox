294c1f4710c310f87e3984af52b2ced2b1b03926
andr9528 <andre@steenhoff.dk>
2018-04-09 12:51:02 +0200

    Made it rady to be pulled in, by out commenting the troublesome clases

--- Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/CustomPickerRenderer.cs
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/CustomPickerRenderer.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using SecurityInAMobile;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.Android;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using SecurityInAMobile;
+//using Xamarin.Forms;
 
-[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
-namespace SecurityInAMobile
-{
-    public class CustomPickerRenderer : PickerRenderer
-    {
-        public CustomPickerRenderer(Context context) : base(context)
-        {
+//[assembly: ExportRenderer(typeof(Picker), typeof(CustomPickerRenderer))]
+//namespace SecurityInAMobile
+//{
+//    public class CustomPickerRenderer : PickerRenderer
+//    {
+//        public CustomPickerRenderer(Context context) : base(context)
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
