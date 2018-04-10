17786846615cce0e10ef81eb41fdbdc950a614d2
Roxana Ion <ionroxana95@yahoo.com>
2018-04-10 14:12:00 +0200

    modified name

--- Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/Login.xaml.cs
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/Login.xaml.cs
@@ -17,7 +17,7 @@ namespace SecurityInAMobile
 			InitializeComponent ();
 		}
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
+        private void LoginButtonClick(object sender, EventArgs e)
         {
             
         }
