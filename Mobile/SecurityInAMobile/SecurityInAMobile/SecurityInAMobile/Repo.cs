ef7afa10d2e320de0bf1d6d13384a82c5012b75d
André Steenhoff Madsen <andre@steenhoff.dk>
2018-04-12 12:21:12 +0200

    saved login box code (#24)
    made the app save the login box code within a xml file stored on the phone, and the disconect remove the saved box code. also added a repo class, for common things that are to be called, primarly the path for the file

---
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/Repo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurityInAMobile
+{
+    public static class Repo
+    {
+        public static string Path;
+        public static string FilePath;
+    }
+}
