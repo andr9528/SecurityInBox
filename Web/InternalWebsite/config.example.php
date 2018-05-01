cd94a0fb93b1a811ada372d35faf7ac82e7e0170
Jonas Laursen <jona8690@edu.eal.dk>
2018-05-01 11:10:08 +0200

    Example config file added

---
+++ Web/InternalWebsite/config.example.php
@@ -0,0 +1,10 @@
+<?php
+	$MySQL = array(
+		"Database" => "Database",
+		"Username" => "User",
+		"Password" => "Passw0rd1",
+		"Server" => "localhost",
+		"Port" => "3306",
+		"Socket" => ""
+	);
+?>
