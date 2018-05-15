6d0585e67d2ed81ca95b1ed419003d611bd86eb9
Liljeroth <chri894u@users.noreply.github.com>
2018-05-15 15:25:13 +0200

    Data Binding
    Added data binding to the History page. Also did major updates to the UI.

--- Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/HistoryPage.xaml.cs
+++ Mobile/SecurityInAMobile/SecurityInAMobile/SecurityInAMobile/HistoryPage.xaml.cs
@@ -9,33 +9,31 @@ using Xamarin.Forms.Xaml;
 
 namespace SecurityInAMobile
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HistoryPage : ContentPage
-	{
-		public HistoryPage()
-		{
-<<<<<<< HEAD
-<<<<<<< HEAD
-			InitializeComponent();
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HistoryPage : ContentPage
+    {
+        public HistoryPage()
+        {
+            InitializeComponent();
             NotificationListView.ItemsSource = notifications;
             PopulateList();
             PutListIntoListView();
-		}
+        }
 
         List<Notification> notifications = new List<Notification>();
 
         public void PopulateList()
         {
-            notifications.Add(new Notification { Name = "Bob", Data = "He's is mad, bro", Seen = true});
-            notifications.Add(new Notification { Name = "Joe & Hugo", Data = "They're are mad, bro", Seen = false});
+            notifications.Add(new Notification { Name = "Bob", Data = "He's is mad, bro", Seen = true });
+            notifications.Add(new Notification { Name = "Joe & Hugo", Data = "They're are mad, bro", Seen = false });
 
             NotificationListView.BindingContext = notifications;
-            
+
             int i = 0;
             while (i < 30)
             {
-                notifications.Add(new Notification { Name = "Hans", Data = "No longer mad, bro", Seen = true});
-                notifications.Add(new Notification { Name = "Franz", Data = "We call him 'no mad'", Seen = false});
+                notifications.Add(new Notification { Name = "Hans", Data = "No longer mad, bro", Seen = true });
+                notifications.Add(new Notification { Name = "Franz", Data = "We call him 'no mad'", Seen = false });
                 i++;
             }
         }
@@ -46,79 +44,6 @@ namespace SecurityInAMobile
             //notifications.Sort();
             ListView NotificationListView = this.FindByName<ListView>("NotificationListView");
             NotificationListView.ItemsSource = notifications;
-=======
-			InitializeComponent ();
-            PopulateNewList();
-            //PutListsIntoListviews();
-        }
-
-        public List<string> newNotificationList = new List<string>();
-        public List<string> oldNotificationList = new List<string>();
-
-        public void PopulateNewList()
-                {
-
-
-                    int i = 0;
-                    while (i < 30)
-                    {
-                        oldNotificationList.Add("Pancake");
-
-                        newNotificationList.Add("Pancake");
-                        i++;
-                    }
-            newNotificationList.Add("hello");
-            newNotificationList.Add("cookie");
-            newNotificationList.Add("completely healthy and alive pony");
-            oldNotificationList.Add("Bacon");
-        }
-
-=======
-			InitializeComponent ();
-            PopulateNewList();
-            //PutListsIntoListviews();
-        }
-
-        public List<string> newNotificationList = new List<string>();
-        public List<string> oldNotificationList = new List<string>();
-
-        public void PopulateNewList()
-                {
-
-
-                    int i = 0;
-                    while (i < 30)
-                    {
-                        oldNotificationList.Add("Pancake");
-
-                        newNotificationList.Add("Pancake");
-                        i++;
-                    }
-            newNotificationList.Add("hello");
-            newNotificationList.Add("cookie");
-            newNotificationList.Add("completely healthy and alive pony");
-            oldNotificationList.Add("Bacon");
-        }
-
->>>>>>> d3ce8b765bc53d5a5ad926277860155f741966c7
-        //        public void PutListsIntoListviews()
-        //        {
-        //            ListView NewNotificationListView = this.FindByName<ListView>("NewNotificationListView");
-        //            NewNotificationListView.ItemsSource = newNotificationList;
-        //            ListView OldNotificationListView = this.FindByName<ListView>("OldNotificationListView");
-        //            OldNotificationListView.ItemsSource = oldNotificationList;
-
-        //        }
-
-        public async Task<List<string>> GetItemsAsync(int pageIndex, int pageSize)
-        {
-            await Task.Delay(2000);
-
-            return newNotificationList.Skip(pageIndex * pageSize).Take(pageSize).ToList();
-<<<<<<< HEAD
->>>>>>> d3ce8b765bc53d5a5ad926277860155f741966c7
-=======
->>>>>>> d3ce8b765bc53d5a5ad926277860155f741966c7
         }
     }
 }
