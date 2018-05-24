df54885f165b76264b1ca930afa9ae623b9e7b09
Roxana Ion <ionroxana95@yahoo.com>
2018-05-24 17:52:29 +0200

    device notification works
    i solved the task with the notification for devices and now I am doing some seetings for devices

--- Web/InternalWebsite/assets/js/deviceData.js
+++ Web/InternalWebsite/assets/js/deviceData.js
@@ -1,46 +1,5 @@
-
-
-function getDevices()
-{
-    var devices = httpGet("/internal/api/getDevices.php");
-    console.log(devices);
-    var devicesData = "";
-    var deviceIndex;
-    
-    for(deviceIndex in devices)
-        {
-            var dataIndex;
-            var device = devices[deviceIndex];
-            var deviceData = ""; 
-            
-            
-            deviceData+= "<td>" + device["Hostname"] + "</td>";
-            
-            deviceData+="<td>"+device["CurIP"]+"</td>";
-            
-            if(device["Status"] == "1")
-                      {
-	                      deviceData += "<td>Approved</td>";
-                      }
-                      else 
-                      {
-	                    deviceData += "<td>Blocked</td>";
-                      }
-            
-            
-            
-            devicesData += "<tr>" + deviceData+ "</tr>";
-            
-        }
-    
-    var currentContent = document.getElementById("table").innerHTML;
-    document.getElementById("table").innerHTML = currentContent + devicesData;
-    
-    
-}
-
-getDevices();
-
+var currentDevice;
+var devices;
 
 function httpGet(theUrl)
 {
@@ -50,10 +9,70 @@ function httpGet(theUrl)
     return JSON.parse(xmlHttp.responseText);
 }
 
+//This function calls the checkDevices() function each second, to see if a new device has been added -- it also calls the checkNewDevice() function to check if a device has been added to the table and display it
+function updateDeviceList()
+{
+    devices = httpGet("/internal/api/getDevices.php");
+    
+    checkNewDevice();
+    getDevices();
+    
+    
+    //setTimeout(updateDeviceList, 1000);
+}
+
+$(document).ready(function(){
+    updateDeviceList();
+});
+
+//This function loops through the devices array and and shows the data I want in the html table, it checks if status i 1 or 2 and posts it as a word "Approved" or "Blocked"
+function getDevices() 
+{
+    var devicesData = "";
+    var deviceIndex;
+    
+    var tableRows = "";
+    for(deviceIndex in devices)
+        {
+            var dataIndex;
+            var device = devices[deviceIndex];
+            var deviceData = ""; 
+            
+            
+            deviceData+= "<td>" + device["Hostname"] + "</td>";
+            
+            deviceData+="<td>" + device["Displayname"] +"</td>";
+            
+            deviceData+="<td>"+device["CurIP"]+"</td>";
+            
+            if(device["Status"] == "1")
+                      {
+	                      deviceData += "<td>Approved</td>";
+                      }
+                      else if(device["Status"]== "2")
+                      {
+	                    deviceData += "<td>Blocked</td>";
+                      }
+                      else
+                      {
+                          deviceData+="<td>Awaiting response</td>";
+                      }
+            
+            
+            deviceData += "<td> &nbsp; <i class='fas fa-trash '></i> &nbsp; &nbsp; <i class='fas fa-edit'></i> </td>";
+            
+            tableRows += "<tr>" + deviceData+ "</tr>";
+
+        }
+    
+    tableRows = "<tr><th>Host Name</th><th>Device Name</th><th>IP Address</th><th>Status</th><th>Device Settings</th></tr>" + tableRows;
+    document.getElementById("table").innerHTML = tableRows;
+}
+
+//This function loops through the devices array and it checks if the status of device is null then notification appears which displays the data of the device with status=null,if device wont have any ip, the ip won't be shown in the notification window
 function checkNewDevice()
 {
-    var devices = httpGet("/internal/api/getDevices.php");
-    console.log(devices);
+    //console.log(devices);
     var devicesData = "";
     var deviceIndex;
     
@@ -65,18 +84,44 @@ function checkNewDevice()
             
             if(device["Status"] == "")
                 {
+                    //this line sets the device in the notification to the current device which will later be used to approve or block the device
+                    currentDevice = device;
                     
                      $(".popup").show();
+                    
+                    deviceNotificationInfo = "Device Name: " + device["Hostname"]+ "<br/>";
+                    
+                    if(device["CurIP"] != "")
+                        {
+                            deviceNotificationInfo+= "IP Address: " + device["CurIP"] + "<br/>";
+                        }
+                    
+                    deviceNotificationInfo+= "Date: " + device["FirstSeen"];
+                    
+                    $('#deviceDataNotification').html(deviceNotificationInfo);                   
+ 
                 }
-    
-        }
-            
-            
-    setTimeout(checkNewDevice, 1000)
-    
-    
+        }           
 }
 
-checkNewDevice();
+//This function sends a post request to the API Endpoint and sets the device status in the database as approved 1, if the request was succesfull the notification will dissapear 
+function approveClick()
+{
+    $.post("api/setDeviceStatus.php", { mac: currentDevice['MAC_Address'], status: "1" } ).done(function(data) {
+		if(data=="200 OK")
+            {
+               $(".popup").hide(); 
+            }
+	});
+}
 
-
+//this function does the same as the one above but this time it sets the status in the database as blocked 2
+function blockClick()
+{
+    $.post("api/setDeviceStatus.php", { mac: currentDevice['MAC_Address'], status: "2" } ).done(function(data) {
+		if(data=="200 OK")
+            {
+               $(".popup").hide(); 
+            }
+	});
+}
